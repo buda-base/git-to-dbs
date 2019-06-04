fd14ffd0959a92c8e2e628b393b0345d2f67b4ea
Chris Tomlinson <ct@moonvine.org>
2019-06-04 12:24:37 -0500

    fix #16 #17 #18; uses bdrc-libraries

--- src/main/java/io/bdrc/gittodbs/EtextContents.java
+++ src/main/java/io/bdrc/gittodbs/EtextContents.java
@@ -1,23 +1,17 @@
 package io.bdrc.gittodbs;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import static io.bdrc.libraries.Models.getFacetNode;
+import static io.bdrc.libraries.Models.FacetType.ETEXT_CHUNK;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.jena.datatypes.xsd.XSDDatatype;
 import org.apache.jena.rdf.model.Model;
-import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.rdf.model.Property;
 import org.apache.jena.rdf.model.Resource;
-
-import io.bdrc.xmltoldmigration.xml2files.CommonMigration;
-import io.bdrc.xmltoldmigration.xml2files.CommonMigration.FacetType;
 
 public class EtextContents {
 
@@ -33,11 +27,6 @@ public class EtextContents {
             this.breakList = breakList;
         }
     }
-//    
-//    public static Model getModel(final String etextId) {
-//        final String filePath = GitToDB.gitDir+"etextcontents/"+TransferHelpers.getMd5(etextId)+"/"+etextId+".txt";
-//        return getModel(filePath, etextId);
-//    }
     
     public static Model getModel(final String filePath, final String etextId, Model etextM) {
         String content = "";
@@ -54,7 +43,6 @@ public class EtextContents {
 
     private static Model getModel(final List<Integer>[] breaks, final int totalStringPointNum, final String totalString, final String etextId,  Model etextM) {
         final Model m = etextM;
-//        final Model m = ModelFactory.createDefaultModel();
         final List<Integer> charBreaks = breaks[0];
         final List<Integer> pointBreaks = breaks[1];
         int chunkSeqNum = 1;
@@ -62,16 +50,13 @@ public class EtextContents {
         int lastPointBreakIndex = 0;
         final Resource etext = m.createResource(TransferHelpers.BDR+etextId);
         final Property hasChunk = m.getProperty(TransferHelpers.BDO, "eTextHasChunk");
-//        final Property seqNum = m.getProperty(TransferHelpers.BDO, "seqNum");
         final Property chunkContents = m.getProperty(TransferHelpers.BDO, "chunkContents");
         for (chunkSeqNum = 1 ; chunkSeqNum <= charBreaks.size() ; chunkSeqNum++) {// final int charBreakIndex : charBreaks) {
             final int charBreakIndex = charBreaks.get(chunkSeqNum-1);
             final int pointBreakIndex = pointBreaks.get(chunkSeqNum-1);
             final String contents = totalString.substring(lastCharBreakIndex, charBreakIndex);
-            Resource chunk = CommonMigration.getFacetNode(FacetType.ETEXT_CHUNK, etext);
-//            Resource chunk = m.createResource();
+            Resource chunk = getFacetNode(ETEXT_CHUNK, etext);
             etext.addProperty(hasChunk, chunk);
-//            chunk.addProperty(seqNum, m.createTypedLiteral(chunkSeqNum, XSDDatatype.XSDinteger));
             chunk.addProperty(chunkContents, m.createLiteral(contents, "bo")); // TODO: what about multilingual etexts?
             chunk.addProperty(m.getProperty(TransferHelpers.BDO, "sliceStartChar"), m.createTypedLiteral(lastPointBreakIndex, XSDDatatype.XSDinteger));
             chunk.addProperty(m.getProperty(TransferHelpers.BDO, "sliceEndChar"), m.createTypedLiteral(pointBreakIndex, XSDDatatype.XSDinteger));
@@ -80,10 +65,8 @@ public class EtextContents {
         }
         if (lastCharBreakIndex != totalString.length()) {
             final String contents = totalString.substring(lastCharBreakIndex);
-            Resource chunk = CommonMigration.getFacetNode(FacetType.ETEXT_CHUNK, etext);
-//            Resource chunk = m.createResource();
+            Resource chunk = getFacetNode(ETEXT_CHUNK, etext);
             etext.addProperty(hasChunk, chunk);
-//            chunk.addProperty(seqNum, m.createTypedLiteral(chunkSeqNum, XSDDatatype.XSDinteger));
             chunk.addProperty(chunkContents, m.createLiteral(contents, "bo"));
             chunk.addProperty(m.getProperty(TransferHelpers.BDO, "sliceStartChar"), m.createTypedLiteral(lastPointBreakIndex, XSDDatatype.XSDinteger));
             chunk.addProperty(m.getProperty(TransferHelpers.BDO, "sliceEndChar"), m.createTypedLiteral(totalStringPointNum, XSDDatatype.XSDinteger));
--- src/test/java/io/bdrc/gittodbs/EtextMergerTest.java
+++ src/test/java/io/bdrc/gittodbs/EtextMergerTest.java
@@ -15,11 +15,8 @@ public class EtextMergerTest {
     @Test
     public void testMerge() throws FileNotFoundException, IOException {
         Model etext = TransferHelpers.modelFromPath("etextmerge/EtextTest-etext.ttl", DocType.ETEXT, "UT1CZ2485_001_0000");
-        Model contents = EtextContents.getModel("src/test/resources/etextmerge/EtextTest-str.txt", "UT1CZ2485_001_0000", etext);
-//        Model contents = EtextContents.getModel("src/test/resources/etextmerge/EtextTest-str.txt", "UT1CZ2485_001_0000");
-//        etext.add(contents);
         
-        etext.write(new FileWriter("/Users/chris/BUDA/NEW_MIGRATION_TESTING/EtextMergerTest-GENERATED.ttl"), "TTL");
+//        etext.write(new FileWriter("/Users/chris/BUDA/NEW_MIGRATION_TESTING/EtextMergerTest-GENERATED.ttl"), "TTL");
         
         Model correctMerge = TransferHelpers.modelFromPath("etextmerge/EtextTest-etext-merged.ttl", DocType.ETEXT, "UT1CZ2485_001_0000");
         TransferHelpers.printModel(etext);
