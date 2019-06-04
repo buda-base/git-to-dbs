700b8e7d65432eaad432f0c880d13cc2e3e1495b
Chris Tomlinson <ct@moonvine.org>
2019-06-04 12:38:49 -0500

    fix EtextMergerTest

--- src/test/java/io/bdrc/gittodbs/EtextMergerTest.java
+++ src/test/java/io/bdrc/gittodbs/EtextMergerTest.java
@@ -3,7 +3,6 @@ package io.bdrc.gittodbs;
 import static org.junit.Assert.assertTrue;
 
 import java.io.FileNotFoundException;
-import java.io.FileWriter;
 import java.io.IOException;
 
 import org.apache.jena.rdf.model.Model;
@@ -15,6 +14,8 @@ public class EtextMergerTest {
     @Test
     public void testMerge() throws FileNotFoundException, IOException {
         Model etext = TransferHelpers.modelFromPath("etextmerge/EtextTest-etext.ttl", DocType.ETEXT, "UT1CZ2485_001_0000");
+        // the following side-effects the etext model just constructed so no need to save in a separste variable
+        EtextContents.getModel("src/test/resources/etextmerge/EtextTest-str.txt", "UT1CZ2485_001_0000", etext);
         
 //        etext.write(new FileWriter("/Users/chris/BUDA/NEW_MIGRATION_TESTING/EtextMergerTest-GENERATED.ttl"), "TTL");
         
